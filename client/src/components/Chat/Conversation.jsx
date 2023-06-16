e1956159473ff73f41ce7fa82308be839f6d987f
Anas Ali <asheryar12@gmail.com>
2023-06-17 00:33:44 +0500

    chat ui

--- client/src/components/Chat/Conversation.jsx
+++ client/src/components/Chat/Conversation.jsx
@@ -1,16 +1,35 @@
-import { Box, IconButton, Text, useDisclosure } from "@chakra-ui/react";
-import React from "react";
+import {
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  Input,
+  Spinner,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import { useSelector } from "react-redux";
 import { checkChat } from "./ChatList";
-import { Eye, Icon } from "iconsax-react";
+import { Eye, Icon, Send2 } from "iconsax-react";
 import GroupSettingsModal from "./GroupSettingsModal";
 
 const Conversation = ({ selectedChat, setSelectedChat }) => {
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
   const { onOpen, onClose, isOpen } = useDisclosure();
   const { users } = useSelector((state) => state.getUserDetailsReducer);
   // console.log(users?.data?.user);
-  console.log(selectedChat);
+  // console.log(selectedChat);
+
+  const handleSendMessage = (e) => {
+    if (e.key === "Enter" && message) {
+      console.log("ok");
+    } else {
+      return;
+    }
+  };
 
   return (
     <>
@@ -39,6 +58,38 @@ const Conversation = ({ selectedChat, setSelectedChat }) => {
             ></IconButton>
           )}
         </Box>
+        <Divider />
+        {selectedChat ? (
+          <>
+            {loading ? (
+              <Spinner size='xl' color='red.500' margin='auto' />
+            ) : (
+              <>
+                <Box display='flex' marginTop='auto' alignItems='center'>
+                  <Input
+                    onKeyDown={handleSendMessage}
+                    placeholder='Enter Message...'
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                  />
+                  <Button colorScheme='teal' marginLeft='5px'>
+                    Send
+                  </Button>
+                </Box>
+              </>
+            )}
+          </>
+        ) : (
+          <Box
+            display='flex'
+            justifyContent='center'
+            alignItems='center'
+            marginTop='auto'
+            marginBottom='auto'
+          >
+            <Text fontSize='2xl'>Please Select a chat</Text>
+          </Box>
+        )}
       </Box>
       <GroupSettingsModal
         isOpen={isOpen}
